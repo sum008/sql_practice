d937ec6df1b6e190e9bc531986264cec276b18df
sum008 <sk4641230@gmail.com>
2023-12-14 20:39:19 +0530

    invert column without using pivot

---
+++ practice_query_47.sql
@@ -0,0 +1,27 @@
+-- invert columns without using pivot
+
+-- 101,english,88
+-- 101,science,90
+-- 101,maths,85
+-- 102,english,70
+-- 102,science,80
+-- 102,maths,83
+
+
+-- create table f_pivot (id int, subject VARCHAR(255), marks INT);
+
+-- LOAD DATA INFILE "C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/f_pivot.csv"
+-- INTO TABLE practice.f_pivot
+-- COLUMNS TERMINATED BY ','
+-- LINES TERMINATED BY '\n'
+-- IGNORE 1 LINES;
+
+select * from f_pivot;
+
+select distinct id, max(english) over(partition by id) as english,
+					max(science) over(partition by id) as science,
+					max(maths) over(partition by id)  as maths from (
+select id, case when subject = 'english' then (select marks from practice.f_pivot where subject='english' and id=f.id) end as english,
+		  case when subject = 'science' then (select marks from practice.f_pivot where subject='science' and id=f.id) end as science,
+          case when subject = 'maths' then (select marks from practice.f_pivot where subject='maths' and id=f.id) end as maths
+from practice.f_pivot f) b;
